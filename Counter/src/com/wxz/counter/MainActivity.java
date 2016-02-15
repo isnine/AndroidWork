e8324b57ba72448f4adaa0e238ba4ceb3567e1bd
Ninse <245328522@qq.com>
2016-02-15 19:00:02 +0800

    计数器最终版本

--- Counter/src/com/wxz/counter/MainActivity.java
+++ Counter/src/com/wxz/counter/MainActivity.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
@@ -12,15 +13,39 @@ public class MainActivity extends Activity {
 	TextView MText;
 	Button Add;
 	Button Sub;
-	
+	int count;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
-
+		
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		
 		MText=(TextView)findViewById(R.id.Countertext);
 		Add=(Button)findViewById(R.id.button1);
 		Sub=(Button)findViewById(R.id.button2);
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
+	   count=0;
+	
+	   Add.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				count++;
+				MText.setText("The total is "+count);
+				
+			}
+		});
+		
+		Sub.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				count--;
+				MText.setText("The total is "+count);
+				
+			}
+		});
+
+
 	}
 
 	@Override
