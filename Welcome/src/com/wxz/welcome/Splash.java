16747159bebfec6730a13e689f8c29c855733e86
Ninse <245328522@qq.com>
2016-02-16 20:00:33 +0800

    欢迎界面V2.0

--- Welcome/src/com/wxz/welcome/Splash.java
+++ Welcome/src/com/wxz/welcome/Splash.java
@@ -1,15 +1,41 @@
 package com.wxz.welcome;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Window;
 
 public class Splash extends Activity{
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		// TODO 自动生成的方法存根
+		requestWindowFeature(Window.FEATURE_NO_TITLE);
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.splash);
+	
+	Thread timer =new Thread(){
+		public void run(){
+			try {
+				sleep(3000);
+			} catch (InterruptedException e) {
+				// TODO 自动生成的 catch 块
+				e.printStackTrace();
+			}finally{
+				Intent openMainActivity=new Intent("android.intent.action.MAINACTIVITY");
+				startActivity(openMainActivity);
+			}
+		}
+	};
+	timer.start();
+	
+	}
+
+	@Override
+	protected void onPause() {
+		// TODO 自动生成的方法存根
+		super.onPause();
+		finish();
 	}
 
 }
