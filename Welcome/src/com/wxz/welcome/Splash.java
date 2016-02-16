c119fe853ae818ccb2a3354593d4323c61f94bf3
Ninse <245328522@qq.com>
2016-02-16 19:41:17 +0800

    安卓欢迎界面v1.0

---
+++ Welcome/src/com/wxz/welcome/Splash.java
@@ -0,0 +1,15 @@
+package com.wxz.welcome;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Splash extends Activity{
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO 自动生成的方法存根
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.splash);
+	}
+
+}
